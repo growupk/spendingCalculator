1c3725c31f075a0900ccfa122b3a9ebb61127c78
BOBISPC\user <bobis.andras@gmail.com>
2017-08-31 21:23:58 +0200

    calculator all

---
+++ app.js
@@ -0,0 +1,56 @@
+/**
+ * Created by user on 2017. 06. 17..
+ */
+$(document).ready(function(){
+
+    $('select').niceSelect()
+
+    //Question not empty
+    $('.sendBtn').click(function(){
+        if( $('input.spend1').val() == '') {
+            $('input.spend1').addClass('empty')
+        }else{
+            $('input.spend1').removeClass('empty')
+        }
+        if($('input.spend2').val() == ''){
+            $('input.spend2').addClass('empty')
+        }else{
+            $('input.spend2').removeClass('empty')
+        }
+        if($('input.spend3').val() == ''){
+            $('input.spend3').addClass('empty')
+        }else{
+            $('input.spend3').removeClass('empty')
+        }
+        if($('input.spend1').val() == '' || $('input.spend2').val() == '' || $('input.spend3').val() == ''){
+            event.preventDefault()
+        }
+    })
+
+    function shake(){
+        var random = Math.floor(Math.random() * 1000)
+        var $li = $(".response i")
+        $li.eq(random % $li.length).toggleClass("shake-chunk shake-slow shake-constant")
+    }
+    setInterval(shake,6000)
+
+    //Mobile view: subtotal move bottom
+
+    var windowWidth = $(window).outerWidth()
+    var subtotal = $('.final-border-box')
+    if(windowWidth < 991){
+        $('.reThird').append(subtotal)
+    }else{
+        $('.coststype').append(subtotal)
+    }
+
+    $(window).on('resize', function(){
+        windowWidth = $(window).outerWidth()
+        subtotal = $('.final-border-box')
+        if(windowWidth < 991){
+            $('.reThird').append(subtotal)
+        }else{
+            $('.coststype').append(subtotal)
+        }
+    })
+})
---
+++ index.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title>Havi költségkalkulátor</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="calculator.css">
+    <link rel="stylesheet" href="shake.css">
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="jquery.nice-select.min.js"></script>
+    <link rel="stylesheet" href="nice-select.css">
+    <script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <script src="app.js"></script>
+</head>
+<body>
+<?php
+    $con = mysql_connect("localhost","root","");
+    if (!$con)
+    {
+        die('Could not connect: ' . mysql_error());
+    }
+
+    $db_select = mysql_select_db("calculator", $con);
+    if (!$db_select) {
+        die("Database selection also failed miserably: " . mysql_error());
+    }
+
+    if($_POST){
+        if(isset($_POST['spend1']) && isset($_POST['spend2']) && isset($_POST['spend3'])){
+        $sql="INSERT INTO cost (costs_type, to_spend_where, to_spend_what, to_spend_price)
+            VALUES
+            ('$_POST[coststype]','$_POST[spend1]','$_POST[spend2]','$_POST[spend3]')";
+            if (!mysql_query($sql,$con))
+            {
+                die('Error: ' . mysql_error());
+            }
+        }
+        //mysql_close($con);
+    }
+?>
+    <header>
+        <!--<h1>Havi Költségkalkulátor</h1>-->
+    </header>
+    <div class="container-fluid questionBox">
+        <form method="post">
+            <div class="row">
+                    <div class="col-12 col-md-6 col-lg-3 questionFields">
+                        <h3><i class="fa fa-line-chart" aria-hidden="true"></i>Kiadás Típusa</h3>
+                        <select name="coststype" id="costs-type">
+                            <option>Étel</option>
+                            <option>Üzemanyag</option>
+                            <option>Albérlet/Rezsi</option>
+                            <option>Luxus</option>
+                            <option>Egyéb/Nem várt</option>
+                        </select>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-3 questionFields">
+                        <h3><i class="fa fa-university" aria-hidden="true"></i>Hol?</h3>
+                        <input type="text" name="spend1" class="spend1">
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-3 questionFields what">
+                        <h3><i class="fa fa-shopping-basket" aria-hidden="true"></i>Mire?</h3>
+                        <input type="text" name="spend2" class="spend2">
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-3 questionFields">
+                        <h3><i class="fa fa-usd" aria-hidden="true"></i>Mennyit?</h3>
+                        <input type="number" name="spend3" class="spend3">
+                    </div>
+            </div>
+            <input type="submit" class="sendBtn">
+        </form>
+    </div>
+    <div class="container-fluid response">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-3 coststype table-container">
+                <h3><i class="fa fa-line-chart" aria-hidden="true"></i>Kiadás típusa</h3>
+                <div class="release-type">
+                    <?php
+                    $results = mysql_query("SELECT costs_type, to_spend_price FROM cost");
+                    $index = 0;
+                    $coType = array();
+                    while($row = mysql_fetch_array($results)){
+                        $coType[$index] = $row['costs_type'];
+                        $index++;
+                        ?>
+
+                        <p class="data-price" data-price="<?php echo $row['to_spend_price']; ?>"><?php  echo $row['costs_type']; ?></p>
+
+                    <?php } ?>
+                        <?php
+                        $food = array();
+                        $fuel = array();
+                        $apartment = array();
+                        $luxx = array();
+                        $other = array();
+
+                        $foodIndex = 0;
+                        $fuelIndex = 0;
+                        $apartmentIndex = 0;
+                        $luxxIndex = 0;
+                        $otherIndex = 0;
+
+                        foreach($coType as $type){
+                            if($type == 'Étel'){
+                                $food[$foodIndex] = $type;
+                                $foodIndex++;
+                            }elseif($type == 'Üzemanyag'){
+                                $fuel[$fuelIndex] = $type;
+                                $fuelIndex++;
+                            }elseif($type == 'Albérlet/Rezsi'){
+                                $apartment[$apartmentIndex] = $type;
+                                $apartmentIndex++;
+                            }elseif($type == 'Luxus'){
+                                $luxx[$luxxIndex] = $type;
+                                $luxxIndex++;
+                            }else{
+                                $other[$otherIndex] = $type;
+                                $otherIndex++;
+                            }
+                        }
+                    if(!empty($food) || !empty($fuel) || !empty($apartment) || !empty($luxx) || !empty($other)){
+                        ?>
+                    <div class="final-border-box">
+                    <?php
+                    }
+                    if(!empty($food)) {
+                        ?>
+                        <p class="final-border">
+                            <?php
+                            echo count($food) . 'x Ételre: ';
+                            ?>
+                            <span class="food"></span>
+                        </p>
+                        <?php
+                    }
+                    if(!empty($fuel)) {
+                        ?>
+                        <p class="final-border">
+                            <?php
+                            echo count($fuel) . 'x Üzemanyagra: ';
+                            ?>
+                            <span class="fuel"></span>
+                        </p>
+                        <?php
+                    }
+                    if(!empty($apartment)){
+                    ?>
+                    <p class="final-border">
+                        <?php
+                        echo count($apartment) . 'x Albérletre/Rezsire: ';
+                        ?>
+                        <span class="apartment"></span>
+                    </p>
+                    <?php
+                    }
+                        if(!empty($luxx)){
+                    ?>
+                        <p class="final-border">
+                            <?php
+                            echo count($luxx) . 'x Luxusra: ';
+                            ?>
+                            <span class="lux"></span>
+                        </p>
+                    <?php
+                        }
+                    ?>
+                    <?php
+                    if(!empty($other)) {
+                        ?>
+                        <p class="final-border">
+                            <?php
+                            echo count($other) . 'x Egyéb/Nem várt: ';
+                            ?>
+                            <span class="other"></span>
+                        </p>
+                        <?php
+                    }
+                    if(!empty($food) || !empty($fuel) || !empty($apartment) || !empty($luxx) || !empty($other)){
+                    ?>
+                    </div>
+                    <?php } ?>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 reFirst table-container">
+                <h3><i class="fa fa-university" aria-hidden="true"></i>Hol</h3>
+                <div>
+                <?php
+                    $results = mysql_query("SELECT to_spend_where FROM cost");
+                    while($row = mysql_fetch_array($results)){
+                ?>
+
+                    <p><?php  echo $row['to_spend_where']; ?></p>
+
+                <?php } ?>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 reSecond table-container">
+                <h3><i class="fa fa-shopping-basket" aria-hidden="true"></i>Mire</h3>
+                <div>
+                <?php
+                    $results = mysql_query("SELECT to_spend_what FROM cost");
+                    while($row = mysql_fetch_array($results)){
+                ?>
+                    <p><?php  echo $row['to_spend_what']; ?></p>
+                <?php } ?>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 reThird table-container">
+                <h3><i class="fa fa-usd" aria-hidden="true"></i>Mennyit</h3>
+                <div>
+                <?php
+                    $subtotal = 0;
+                    $results = mysql_query("SELECT to_spend_price FROM cost");
+                    while($row = mysql_fetch_array($results)){
+                ?>
+                    <p><?php  echo $row['to_spend_price'] . ' Ft'; ?></p>
+                <?php
+                    $subtotal += $row['to_spend_price'];
+                    }
+                    //mysql_close($con);
+                ?>
+                <p><?php  echo 'Összesen: ' . $subtotal . ' Ft'; ?></p>
+
+                <?php
+                    $subtotalPercent = $subtotal/100;
+                ?>
+                <script>
+                    //Cake diagramm
+                    $(document).ready(function(){
+                    var allSpended = '<?php echo $subtotal; ?>'
+
+                    var release_type = $('.data-price').map(function() {
+                        return $(this);
+                    }).get();
+
+                    var foodPrices = 0
+                    var fuelPrices = 0
+                    var housePrices = 0
+                    var luxPrices = 0
+                    var somePrices = 0
+
+
+                    for(var i=0; i<release_type.length; i++){
+                        if(release_type[i].text() == 'Étel'){
+                            foodPrices += parseInt(release_type[i].attr('data-price'))
+                        }
+                        if(release_type[i].text() == 'Üzemanyag'){
+                            fuelPrices += parseInt(release_type[i].attr('data-price'))
+                        }
+                        if(release_type[i].text() == 'Albérlet/Rezsi'){
+                            housePrices += parseInt(release_type[i].attr('data-price'))
+                        }
+                        if(release_type[i].text() == 'Luxus'){
+                            luxPrices += parseInt(release_type[i].attr('data-price'))
+                        }
+                        if(release_type[i].text() == 'Egyéb/Nem várt'){
+                            somePrices += parseInt(release_type[i].attr('data-price'))
+                        }
+                    }
+                    $('.final-border-box .food').text(foodPrices + ' Ft')
+                    $('.final-border-box .fuel').text(fuelPrices + ' Ft')
+                    $('.final-border-box .apartment').text(housePrices + ' Ft')
+                    $('.final-border-box .lux').text(luxPrices + ' Ft')
+                    $('.final-border-box .other').text(somePrices + ' Ft')
+                    var morePriceArray = []
+                    var foodPercent = foodPrices/allSpended*100
+                    var fuelPercent = fuelPrices/allSpended*100
+                    var housePercent = housePrices/allSpended*100
+                    var luxPercent = luxPrices/allSpended*100
+                    var somePercent = somePrices/allSpended*100
+
+                    morePriceArray.push(foodPercent,fuelPercent,housePercent,luxPercent,somePercent)
+                    var morePrice = Math.max.apply( Math, morePrice )
+
+                    var chart = new CanvasJS.Chart("chartContainer",
+                        {
+                            title:{
+                                text: "Havi kiadások eddigi alakulása"
+                            },
+                            exportFileName: "Pie Chart",
+                            exportEnabled: true,
+                            animationEnabled: true,
+                            legend:{
+                                verticalAlign: "bottom",
+                                horizontalAlign: "center"
+                            },
+                            data: [
+                                {
+                                    type: "pie",
+                                    showInLegend: true,
+                                    toolTipContent: "{name}: <strong>{y}%</strong>",
+                                    indexLabel: "{name} {y}%",
+                                    dataPoints: [
+                                        {  y: foodPercent.toFixed(2), name: "Étel", exploded: true},
+                                        {  y: fuelPercent.toFixed(2), name: "Üzemanyag"},
+                                        {  y: housePercent.toFixed(2), name: "Albérlet/Rezsi"},
+                                        {  y: luxPercent.toFixed(2), name: "Luxus"},
+                                        {  y: somePercent.toFixed(2),  name: "Egyéb/Nem várt"}
+                                    ]
+                                }
+                            ]
+                        });
+                    chart.render();
+                    })
+                </script>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="chartContainer" style="height: 500px; width: 100%;"></div>
+    <?php
+        if($_POST){
+            if(isset($_POST['deleteAll'])){
+                mysql_query( "DELETE FROM cost" );
+            }
+        }
+    ?>
+    <form method="post">
+        <input type="submit" name="deleteAll" class="deleteBtn" value="DB Töröl">
+    </form>
+</body>
+</html>
